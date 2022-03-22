2d58292f5be1bee7fda8a39f8b6dc8c702445abd
Van6242 <qhart52336@gmail.com>
2022-03-22 10:26:42 -0400

    Initial Commit

---
+++ not a scam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  not a scam
+//
+//  Created by  on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
